f43ef526c87929f57e725e314ebc4ae27c7ab72f
Thomas Ballinger <thomasballinger@gmail.com>
2015-06-03 15:04:50 -0400

    add arity checking to LazyCanvas methods
    Potential issues: multiarity methods of context

--- LazyCanvas.js
+++ LazyCanvas.js
@@ -1,16 +1,27 @@
 ;(function() {
   'use strict';
 
-  function LazyCanvas(canvasId){
+  function LazyCanvas(canvasId, lazy){
+    if (lazy === undefined){
+      lazy = false;
+    }
     this.canvas = document.getElementById(canvasId);
     this.ctx = canvas.getContext('2d');
-    this.lazy = false;
+    this.lazy = lazy;
     this.operations = [];
 
     for (var property in this.ctx){
       this[property] = function(property){
         return function(){
-          this.operations.push([this.ctx[property], Array.prototype.slice.call(arguments)]);
+          var method = this.ctx[property];
+          var args = Array.prototype.slice.call(arguments);
+          if (method.length !== args.length){
+            throw Error(method+' called with '+args.length+' parameters, expected '+method.length);
+          }
+          this.operations.push([method, args]);
+          if (this.lazy){
+            this.trigger();
+          }
         };
       }(property);
     }
@@ -19,7 +30,7 @@
     while (this.operations.length){
       var operation = this.operations.shift();
       console.log("running", operation);
-      operation[0].apply(this.ctx, operation[1])
+      operation[0].apply(this.ctx, operation[1]);
     }
   };
 
