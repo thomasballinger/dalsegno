6fc174e1fdf2877115bd15cb1a165af0cdf93ea5
Thomas Ballinger <thomasballinger@gmail.com>
2016-04-05 20:35:37 -0400

    new tests failing for bytecode, juicy bug

--- parse.js
+++ parse.js
@@ -89,7 +89,7 @@
     }
     var result = innerParse(s);
     if (s.length !== 0) {
-      throw new Error("Didn't finish parse of "+s);
+      throw new Error("Didn't finish parse of "+justContent(s));
     }
     return result;
   }
--- stdlib.js
+++ stdlib.js
@@ -16,34 +16,15 @@
 
   var builtins = require('./builtins.js');
   var run = require('./run');
-  var Immutable = require('./Immutable');
   var Environment = require('./Environment');
+  var stdlibcode = require('./stdlibcode');
 
   var env = new Environment([new Environment.Scope(builtins), new Environment.Scope()], null);
 
   // Use lambdas so snapshots aren't tracked of them
   // Don't create any funs here! They won't work.
 
-  run("(define reduce (lambda func arr initial\n"+
-      "  (if (= (length arr) 0)\n"+
-      "      initial\n"+
-      "      (func (reduce func (rest arr) initial) (first arr)))))",
-      env);
-
-  run("(define filter (lambda func arr\n"+
-      "  (reduce \n"+
-      "    (lambda acc item (if (func item)\n"+
-      "                     (prepend item acc)\n"+
-      "                     acc))\n"+
-      "    arr (list))))",
-      env);
-
-  run("(define map (lambda func arr\n"+
-      "  (if (= (length arr) 0)\n"+
-      "      (list)\n"+
-      "      (prepend (func (first arr)) (map func (rest arr))))))",
-      env);
-
+  stdlibcode.forEach( code => run(code, env));
 
   var stdlib = env.scopes[1].data;
 
