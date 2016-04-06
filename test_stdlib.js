8a7df5ceaec40559fb6d5fcfe7ea287ed151eefe
Thomas Ballinger <thomasballinger@gmail.com>
2016-04-06 13:40:33 -0400

    fix stdlib tests

--- test_stdlib.js
+++ test_stdlib.js
@@ -2,30 +2,41 @@
 var chai = require('chai');
 var assert = chai.assert;
 
-var run = require('./run');
 var Environment = require('./Environment.js');
-var stdlib = require('./stdlib');
+var stdlib = require('./stdlib.js');
+var bcstdlib = require('./bcstdlib.js');
 var builtins = require('./builtins');
+var evalgenrun = require('./run');
+var bcrun = require('./bcrun');
 
-var env = new Environment([new Environment.Scope(builtins), new Environment.Scope(stdlib)]);
 
 describe('stdlib', function(){
-  describe('reduce', function(){
-    it('should sum numbers', function(){
-      assert.deepEqual(run('(reduce (lambda a b (+ a b)) (list 1 2 3) 0)', env), 6);
-    });
-  });
-  describe('filter', function(){
-    it('should passthrough', function(){
-      assert.deepEqual(run('(filter (lambda a 1) (list 1 2 3))', env).toJS(), [1, 2, 3]);
-    });
-  });
-});
+  var tests = function(run, stdlib){
+    return function(){
+      var env = new Environment([new Environment.Scope(builtins), new Environment.Scope(stdlib)]);
 
-describe('builtins', function(){
-  describe('nth', function(){
-    it('should index', function(){
-      assert.deepEqual(run('(nth 0 (list 1 2 3))', env), 1);
-    });
-  });
+      describe('integration', function(){
+        describe('reduce', function(){
+          it('should sum numbers', function(){
+            assert.deepEqual(run('(reduce (lambda a b (+ a b)) (list 1 2 3) 0)', env), 6);
+          });
+        });
+        describe('filter', function(){
+          it('should passthrough', function(){
+            assert.deepEqual(run('(filter (lambda a 1) (list 1 2 3))', env).toJS(), [1, 2, 3]);
+          });
+        });
+      });
+
+      describe('builtins', function(){
+        describe('nth', function(){
+          it('should index', function(){
+            assert.deepEqual(run('(nth 0 (list 1 2 3))', env), 1);
+          });
+        });
+      });
+    };
+  };
+  describe('with evalgen', tests(evalgenrun, stdlib));
+  describe('with bytcode', tests(bcrun, bcstdlib));
 });
