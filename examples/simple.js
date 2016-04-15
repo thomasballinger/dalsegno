ae9b9b61abc312b0d05276a00d3dec3a96aa6d5c
Thomas Ballinger <thomasballinger@gmail.com>
2016-04-15 12:26:25 -0400

    add demo as simple example script

---
+++ examples/simple.js
@@ -0,0 +1,16 @@
+// vim: set ft=scheme:
+window.simpleProgram = `;semicolons make the rest of a line a comment
+(define x 10)           ;var x = 10
+(defn recur ()          ;named function definitions are global
+  (color 100 200 100)   ;sets the color to be used
+                        ;for future draw operations
+  (fillRect 0 0 width height) ;this is the canvas
+                        ;context drawing operation
+  (color 0 0 230)
+  (drawArc x 100 111)   ;queues a circle to be drawn
+  (render)              ;actually paints queued drawings
+  (set! x (+ x .1))     ;change var wherever it was defined
+  (if (> x 300)         ;you've got if, set!, define, defn,
+      (set! x 0))       ;and lambda - that's it for special forms
+  (recur))              ;no loop constructs - you have to recur!
+(recur)`;
