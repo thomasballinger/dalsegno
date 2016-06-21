0e7fd3c3fdb29713d3e9104b74bbc906cbba41f4
Thomas Ballinger <thomasballinger@gmail.com>
2016-06-20 20:34:02 -0400

    accelerating sampler works for some values

--- framesample.js
+++ framesample.js
@@ -1,6 +1,11 @@
 ;(function() {
   'use strict';
 
+  /** Functions which, given a list of numbers, return a subset
+   * of those numbers in the same order.
+   * (or factories for such functions)
+   */
+
   function makeRandomSampler(targetNumFrames){
     return function(frames){
       var targetNumFrames = 100;
@@ -14,14 +19,51 @@
     };
   }
 
-  function makeSlowFastSlowSampler(targetNumFrames){
+  /** an accelerating then decelerating effect */
+  function makeAcceleratingSampler(targetNumFrames){
     return function(frames){
+      if (frames.length <= targetNumFrames){ return frames; }
+      if (targetNumFrames === 0){ return []; }
+      if (targetNumFrames === 1){ return [frames[0]]; }
+      if (targetNumFrames === 2){ return [frames[0], frames[frames.length-1]]; }
+
+      // by this point there are at least 4 frames and we want 3 or more
+      if (!(targetNumFrames >= 3 && frames.length >= 4)){ throw Error('assertion error'); }
+
+      var n = frames.length;
+      var m = targetNumFrames;
+      // using increasing skips,
+      // it's easy to remove 0, 1, 3, 6, 10, ... frames
+      // (triangular numbers, OEIS A000217)
+      // Let's find the largest triangular number t such that
+      // n - t >= m
+      var t = 0, i = 0;
+      while(n - (t+i) >= m){
+        t = t+i;
+        i++;
+      }
+      var largestSkip = i-1;
+
+      frames.reverse();
+      var newFrames = [];
+      var index = 0;
+      for (i=largestSkip+1; i>0; i--){
+        newFrames.push(frames[index]);
+        index += i;
+      }
+      newFrames = newFrames.concat(frames.slice(index));
+      //TODO now we should take every other from the front or something
+      //until we have the target number
+      newFrames.reverse();
+      return newFrames;
     };
   }
 
+  makeAcceleratingSampler(3)([1,2,3,4,5,6,7,8,9]);
+
   var samplers = {};
   samplers.makeRandomSampler = makeRandomSampler;
-  samplers.makeSlowFastSlowSampler = makeSlowFastSlowSampler;
+  samplers.makeAcceleratingSampler = makeAcceleratingSampler;
 
 
   if (typeof exports !== 'undefined') {
--- test_framesample.js
+++ test_framesample.js
@@ -12,4 +12,47 @@ describe('frame samplers', ()=>{
       assert.equal(sampler(input).length, input.length);
     });
   });
+  describe('acceleratingSampler', ()=>{
+    function takeN(input, n, expected){
+      var sampler = framesample.makeAcceleratingSampler(n);
+      var result = sampler(input);
+      assert.deepEqual(result, expected);
+    }
+    it('uses all frames if target num less that input', ()=>{
+      takeN([1, 4, 6, 10, 12, 13], 10, [1, 4, 6, 10, 12, 13]);
+      takeN([1, 2, 3, 4, 5, 6, 7], 9, [1, 2, 3, 4, 5, 6, 7]);
+      takeN([1, 2, 3, 4, 5, 6, 7], 7, [1, 2, 3, 4, 5, 6, 7]);
+    });
+    it('skips appropriately for removing triangular numbers', ()=>{
+      takeN([1, 2, 3, 4, 5, 6, 7], 6, [1, 2, 3, 4, 5, 7]);
+      takeN([1, 2, 3, 4, 5, 6, 7], 4, [1, 2, 4, 7]);
+      takeN([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4, [1, 3, 6, 10]);
+      takeN([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4, [1, 3, 6, 10]);
+    });
+    it('skips appropriately for removing non-triangular numbers', ()=>{
+      takeN([1, 2, 3, 4, 5, 6, 7], 5, [1, 2, 3, 5, 7]);
+    });
+  });
+  /*
+  describe('fastSlowFastSampler', ()=>{
+    it('preserves all frames if target num less that input', ()=>{
+      var sampler = framesample.makeSlowFastSlowSampler(10);
+      var input = [1, 4, 6, 10, 12, 13];
+      assert.equal(sampler(input).length, input.length);
+    });
+    it('tosses the middleish ones', ()=>{
+      function takeN(input, n, expected){
+        var sampler = framesample.makeSlowFastSlowSampler(n);
+        var result = sampler(input);
+        assert.deepEqual(result, expected);
+      }
+      takeN([1, 2, 3, 4, 5, 6, 7], 9, [1, 2, 3, 4, 5, 6, 7]);
+      takeN([1, 2, 3, 4, 5, 6, 7], 7, [1, 2, 3, 4, 5, 6, 7]);
+      takeN([1, 2, 3, 4, 5, 6, 7], 5, [1, 2, 4, 6, 7]);
+      takeN([1, 2, 3, 4, 5, 6, 7], 3, [1, 4, 7]);
+      takeN([1, 2, 3, 4, 5, 6, 7], 2, [1, 7]);
+      takeN([1, 2, 3, 4, 5, 6, 7], 4, [1, 3, 5, 7]);
+    });
+  });
+  */
 });
