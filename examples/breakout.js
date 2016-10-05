b990f0f62dcb3ffc9f9e378b37d07edda21614f0
Thomas Ballinger <thomasballinger@gmail.com>
2016-10-05 16:51:36 -0400

    more design

--- examples/breakout.js
+++ examples/breakout.js
@@ -1,19 +1,24 @@
 // vim: set ft=scheme:
-window.breakoutProgram = `(define paddle-x 100)
-(define paddle-y (- height 20))
+window.breakoutProgram = `(define paddle-y (- height 20))
 (define ball-x 100)
 (define ball-y (- height 50))
 (define dx 1)
 (define dy 1)
 (define paddle-w 100)
 
-(defn main ()
-  (move 1)
-  (draw)
-  (main))
+(defn draw ()
+  (color 130 200 230)
+  (fillRect 0 0 width height)
+  (color 200 0 100)
+  (fillRect (- paddle-x (/ paddle-w 2))
+            paddle-y paddle-w 10)
+  (color 40 40 100)
+  (fillRect (- ball-x 5) (- ball-y 5) 10 10)
+  (render))
 
 (defn bounce ()
   (set! dy (- 0 dy)))
+
 
 (defn move (speed)
   (set! paddle-x (mousex))
@@ -35,14 +40,11 @@ window.breakoutProgram = `(define paddle-x 100)
       (set! ball-x 100)
       (set! ball-y 100))))
 
-(defn draw ()
-  (color 130 200 230)
-  (fillRect 0 0 width height)
-  (color 200 0 100)
-  (fillRect (- paddle-x (/ paddle-w 2))
-            paddle-y paddle-w 10)
-  (color 40 40 100)
-  (fillRect (- ball-x 5) (- ball-y 5) 10 10)
-  (render))
+(define paddle-x)
+
+(defn main ()
+  (move 1)
+  (draw)
+  (main))
 
 (main)`;
--- examples/simple.js
+++ examples/simple.js
@@ -1,7 +1,7 @@
 // vim: set ft=scheme:
 window.simpleProgram = `;semicolons make the rest of a line a comment
-(define y 10)           ;for JSers, that's var y = 10
-(defn recur ()          ;defn creates a global function
+(define y 10)           ;define y and set it to 10
+(defn recur ()          ;defn defines a global function
   (color 100 200 100)   ;sets the color to be used
                         ;for future draw operations
   (fillRect 0 0 width height) ;this is the canvas
@@ -10,7 +10,7 @@ window.simpleProgram = `;semicolons make the rest of a line a comment
   (drawArc 150 y 111)   ;queues a circle to be drawn
   (render)              ;actually paints queued drawings
   (set! y (+ y 5))      ;update the value of y
-  (if (> y 300)         ;you've got if, set!, define, defn,
-      (set! y 0))       ; and lambda for special forms
+  (if (> y 300)         ;if, set!, define, defn, and
+      (set! y 0))       ; lambda are special forms
   (recur))              ;no loops; recurse, recurse!
 (recur)`;
