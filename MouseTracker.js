2c2e7c72d7d78a646b60da406da29fb4635868ac
Thomas Ballinger <thomasballinger@gmail.com>
2016-10-09 21:38:04 -0400

    remember mouse clicks for 50ms
    Since adding the frame limit (and before to some degre as well)
    the is-mouse-down? style call (clicked) sometimes missed mousepresses
    because the mouse would be pressed while the code was at a different
    part of the program or not running and was no longer pressed by the
    time the (clicked) call happened. Now a mousedown any time in the last
    50ms will count towards (clicked) the first time it is called, but if
    clicked is called again it'll return the real value of whether the mouse
    is actually down or not, until another mouse click happens to reset the
    50ms timer.
    
    This doesn't feel like cheating too much because it's called "clicked",
    not "button-down-now".

--- MouseTracker.js
+++ MouseTracker.js
@@ -7,6 +7,7 @@ function MouseTracker(canvasId){
   this.canvas = document.getElementById(canvasId);
   var mouse = this.mouse = {x: 0, y: 0};
   var mousedown = this.mousedown = [false];
+  var lastClick = this.lastClick = [0];
 
   Object.defineProperty(this, '_is_nondeterministic', {
     enumerable: false,
@@ -20,6 +21,7 @@ function MouseTracker(canvasId){
   }, false);
   this.canvas.addEventListener('mousedown', function(e){
     mousedown[0] = true;
+    lastClick[0] = (new Date()).getTime();
   }, false);
   this.canvas.addEventListener('mouseup', function(e){
     mousedown[0] = false;
@@ -42,7 +44,16 @@ MouseTracker.prototype.mousey = function(){
   return this.mouse.y;
 };
 MouseTracker.prototype.clicked = function(){
-  return this.mousedown[0];
+  if (this.mousedown[0]){
+    return true;
+  }
+  // This way even at 20 FPS a click is certain to be picked up
+  var now = (new Date()).getTime();
+  if (now - this.lastClick[0] < 50){
+    this.lastClick[0] = 0;
+    return true;
+  }
+  return false;
 };
 
 MouseTracker.MouseTracker = MouseTracker;
